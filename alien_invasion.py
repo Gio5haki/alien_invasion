d49840657bbdbe6d04f14fef2c9d2a5b79c0417d
Giorgi Shakiashvili <giorgishakiashvili@gmail.com>
2020-10-20 20:18:56 +0400

    added start game button

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from time import sleep
 import pygame
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -30,18 +31,21 @@ class AlienInvasion:
 
         self._create_fleet()
 
+        #Make the Play button
+        self.play_button = Button(self, "Play")
+
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             self._check_events()
 
-        if self.stats.game_active:
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
 
-        self._update_screen()
+            self._update_screen()
 
 
     def _check_events(self):
@@ -53,6 +57,15 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+    
+
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the player clicks Play"""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
 
 
     def _check_keydown_events(self, event):
@@ -171,6 +184,11 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        #Draw the Play button if the game is inactive
+        if not self.stats.game_active:
+            self.play_button.draw_button()
+
         pygame.display.flip()
 
 
@@ -190,6 +208,7 @@ class AlienInvasion:
 
             #Pause
             sleep(0.5)
+            
         else: 
             self.stats.game_active = False
 
--- game_stats.py
+++ game_stats.py
@@ -4,8 +4,8 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
-        #Start Alien Invastion in in an active state
-        self.game_active = True
+        #Start game in an inactive state
+        self.game_active = False
     
     def reset_stats(self):
         """Initialise statistics that can change during the game"""
